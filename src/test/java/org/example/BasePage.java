68bcf04e314e78f63516f2198fc18d4cc34f84fb
ELENAQQQ <kuzheleva.elena@mail.ru>
2022-12-02 12:46:34 +0100

    modified pom.xml, AppTest, BasePage

---
+++ src/test/java/org/example/BasePage.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+
+public class BasePage {
+
+    //Здесь будут все локаторы
+
+// Реализовать метод открытия здесь, перенести сюда загрузку вебдрайвера
+
+//    public void open () {
+//
+//    }
+
+
+}
