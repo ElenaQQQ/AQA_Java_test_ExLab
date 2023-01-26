e8cc489ac75a5eb7e8ef685e7616abe732ca36fe
ELENAQQQ <kuzheleva.elena@mail.ru>
2023-01-26 19:31:26 +0100

    headless mode

--- src/main/java/Utils/Config.java
+++ src/main/java/Utils/Config.java
@@ -1,7 +1,15 @@
 package Utils;
 
+import org.openqa.selenium.Dimension;
+
 public class Config {
     public static final Integer THREAD_SLEEP_WAIT = 1000;
+    public static final Integer IMPLICITY_WAIT = 5;
+    public static final Integer SCRIPT_TIMEOUT = 10;
+    public static final Integer PAGE_LOAD_TIMEOUT = 10;
     public static final String BASE_URL = "http://test.exlab.team/";
     public static final Boolean ON_HEADLESS = true;
+    public static final Integer width = 1680;
+    public static final Integer height = 1050;
+    public static final Dimension DIMENSION = new Dimension(width, height);
 }
--- src/main/java/driver/driver.java
+++ src/main/java/driver/driver.java
@@ -3,9 +3,12 @@ package driver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 
 import java.time.Duration;
+
+import static Utils.Config.*;
 
 public class driver {
 
@@ -14,14 +17,13 @@ public static Actions actions;
 
 public static void createDriver(){
     WebDriverManager.chromedriver().setup();
-    driver = new ChromeDriver();
+    driver = new ChromeDriver(getChromeOptions());
 
-    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-    driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(10));
-    driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
-
-//    actions = new Actions(driver);
-    driver.manage().window().maximize();
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(IMPLICITY_WAIT));
+    driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(SCRIPT_TIMEOUT));
+    driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(PAGE_LOAD_TIMEOUT));
+    driver.manage().window().setSize(DIMENSION);
+//    driver.manage().window().maximize();
 }
 
 public static WebDriver getDriver (){
@@ -31,6 +33,14 @@ public static WebDriver getDriver (){
 public static void quite() {
 //        driver.close();
         driver.quit();
+}
+
+protected static ChromeOptions getChromeOptions(){
+    ChromeOptions chromeOptions = new ChromeOptions();
+        if(ON_HEADLESS) {
+            chromeOptions.addArguments("--headless");
+        }
+        return chromeOptions;
 }
 
 }
